0537c4b68e0c5ffe4af90a7d0dbcaa1e922af025
Pelekon <pelekon@gmail.com>
2019-12-05 18:24:38 +0100

    Init repo with working WIP version.

---
+++ RCArmorTokenData.lua
@@ -0,0 +1,89 @@
+﻿-- Author      : Potdisc
+-- Create Date : 3/11/2013 10:25:13 PM
+-- RCArmorTokenData.lua
+-- Contains itemIDs and their equivalent equip location
+
+RCTokenTable = {
+	--[xxxxxx] = "ExampleSlot",
+
+	-- TIER 7 Naxx, OS, EoE
+	[40616] = "HeadSlot", -- priest, pala, lock 10
+	[40631] = "HeadSlot", -- priest, pala, lock 25
+	[40617] = "HeadSlot", -- warrior, hunter, shammy 10
+	[40631] = "HeadSlot", -- warrior, hunter, shammy 25
+	[40618] = "HeadSlot", -- rogue, DK, mage, druid 10
+	[40633] = "HeadSlot", -- rogue, DK, mage, druid 10
+
+	[40610] = "ChestSlot", -- priest, pala, lock 10
+	[40625] = "ChestSlot", -- priest, pala, lock 25
+	[40611] = "ChestSlot", -- warrior, hunter, shammy 10
+	[40626] = "ChestSlot", -- warrior, hunter, shammy 25
+	[40612] = "ChestSlot", -- rogue, DK, mage, druid 10
+	[40627] = "ChestSlot", -- rogue, DK, mage, druid 25
+
+	[40622] = "ShoulderSlot", -- priest, pala, lock 10
+	[40637] = "ShoulderSlot", -- priest, pala, lock 25
+	[40623] = "ShoulderSlot", -- warrior, hunter, shammy 10
+	[40638] = "ShoulderSlot", -- warrior, hunter, shammy 10
+	[40624] = "ShoulderSlot", -- rogue, DK, mage, druid 10
+	[40639] = "ShoulderSlot", -- rogue, DK, mage, druid 25
+
+	[40619] = "LegsSlot", -- priest, pala, lock 10
+	[40619] = "LegsSlot", -- priest, pala, lock 25
+	[40620] = "LegsSlot", -- warrior, hunter, shammy 10
+	[40635] = "LegsSlot", -- warrior, hunter, shammy 25
+	[40621] = "LegsSlot", -- rogue, DK, mage, druid 10
+	[40636] = "LegsSlot", -- rogue, DK, mage, druid 25
+
+	[40613] = "HandsSlot", -- priest, pala, lock 10
+	[40628] = "HandsSlot", -- priest, pala, lock 25
+	[40614] = "HandsSlot", -- warrior, hunter, shammy 10
+	[40629] = "HandsSlot", -- warrior, hunter, shammy 25
+	[40615] = "HandsSlot", -- rogue, DK, mage, druid 10
+	[40630] = "HandsSlot", -- rogue, DK, mage, druid 10
+
+
+	 -- TIER 8 - Ulduar -------------------------------
+		-- Normal
+	[45635] = "ChestSlot",
+	[45636] = "ChestSlot",
+	[45637] = "ChestSlot",
+
+	[45647] = "HeadSlot",
+	[45648] = "HeadSlot",
+	[45649] = "HeadSlot",
+
+	[45644] = "HandsSlot",
+	[45645] = "HandsSlot",
+	[45646] = "HandsSlot",
+
+	[45650] = "LegsSlot",
+	[45651] = "LegsSlot",
+	[45652] = "LegsSlot",
+
+	[45659] = "ShoulderSlot",
+	[45660] = "ShoulderSlot",
+	[45661] = "ShoulderSlot",
+
+		-- Heroic
+	[45632] = "ChestSlot",
+	[45633] = "ChestSlot",
+	[45634] = "ChestSlot",
+
+	[45638] = "HeadSlot",
+	[45639] = "HeadSlot",
+	[45640] = "HeadSlot",
+
+	[45641] = "HandsSlot",
+	[45642] = "HandsSlot",
+	[45643] = "HandsSlot",
+
+	[45653] = "LegsSlot",
+	[45654] = "LegsSlot",
+	[45655] = "LegsSlot",
+
+	[45656] = "ShoulderSlot",
+	[45657] = "ShoulderSlot",
+	[45658] = "ShoulderSlot",
+}
+
