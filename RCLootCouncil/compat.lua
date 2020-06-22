0c7a2b835b7f0928be85056bc49322dea583303b
Andrew <andrewseward93@gmail.com>
2020-06-22 03:08:14 -0700

    fix compat

--- RCLootCouncil/compat.lua
+++ RCLootCouncil/compat.lua
@@ -1,5 +1,5 @@
 -- get num group members backport
-if not GetNumGroupMembers() then
+if not GetNumGroupMembers then
     function GetNumGroupMembers()
         if IsInRaid() then 
             return GetNumRaidMembers()
